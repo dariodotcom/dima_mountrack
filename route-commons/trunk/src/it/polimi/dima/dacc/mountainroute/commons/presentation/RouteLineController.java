d891a1346ba87e70b60eba4f3b42c286e81e96aa
dariodotcom <dariodotcom@f96df3ad-30d3-41b2-a700-6b3b9752d488>
2013-11-12 20:11:55 +0000

    Move some logic to RouteStepper

--- route-commons/trunk/src/it/polimi/dima/dacc/mountainroute/commons/presentation/RouteLineController.java
+++ route-commons/trunk/src/it/polimi/dima/dacc/mountainroute/commons/presentation/RouteLineController.java
@@ -5,43 +5,25 @@ import it.polimi.dima.dacc.mountainroute.commons.types.Route;
 import it.polimi.dima.dacc.mountainroute.commons.utils.Logger;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-
-import android.util.Log;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Polyline;
 
 public class RouteLineController {
-	private LinkedList<LatLng> traversedList, pendingList;
 	private Polyline traversed, pending;
 	private Logger log;
-
-	private int currentEdge;
-	private int routeSize;
-	private boolean hasInterpolationPoint;
+	private RouteStepper stepper;
 
 	public RouteLineController(Polyline traversed, Polyline pending,
 			Route route, float completeIndex) {
-		List<LatLng> points = convertPointList(route.getRoute());
 		this.pending = pending;
 		this.traversed = traversed;
-		this.currentEdge = (int) Math.floor(completeIndex);
-		this.routeSize = points.size();
-		this.hasInterpolationPoint = false;
 		this.log = new Logger("ROUTE_LINE_CONTROLLER");
 
-		if (currentEdge >= routeSize) {
-			throw new IllegalArgumentException("index must be < " + routeSize);
-		}
-
-		traversedList = new LinkedList<LatLng>(points.subList(0,
-				currentEdge + 1));
-		pendingList = new LinkedList<LatLng>(points.subList(currentEdge + 1,
-				routeSize));
-
-		setCompleteIndex(completeIndex);
+		// Load stepper
+		List<LatLng> routeLatLng = convertPointList(route.getRoute());
+		this.stepper = new RouteStepper(routeLatLng, completeIndex);
 	}
 
 	public RouteLineController(Polyline traversed, Polyline pending, Route route) {
@@ -49,58 +31,14 @@ public class RouteLineController {
 	}
 
 	public void setCompleteIndex(float index) {
-		checkInputIndex(index);
-
-		// Remove the interpolation point from lists
-		if (hasInterpolationPoint) {
-			log.d("Remove interpolation point");
-			pendingList.removeFirst();
-			traversedList.removeLast();
-			hasInterpolationPoint = false;
-		}
-
-		// Step to next point if necessary
-		int newEdge = (int) Math.floor(index);
-		if (newEdge > currentEdge) {
-			LatLng edge = pendingList.removeFirst();
-			traversedList.addLast(edge);
-			currentEdge = newEdge;
-			log.d("New edge index: " + newEdge);
-		}
-
-		// Add interpolation point if necessary
-		float coeff = index - currentEdge;
-		if (coeff > 0) {
-			LatLng interpolated = linearInterpolate(traversedList.getLast(),
-					pendingList.getFirst(), coeff);
-			Log.d("ROUTE", "Interpolated point: " + interpolated);
-			traversedList.addLast(interpolated);
-			pendingList.addFirst(interpolated);
-			hasInterpolationPoint = true;
-			log.d("New interpolation point: " + interpolated);
-		}
-
+		stepper.step(index);
 		updateLines();
 	}
 
 	private void updateLines() {
-		traversed.setPoints(traversedList);
-		pending.setPoints(pendingList);
+		traversed.setPoints(stepper.getTraversed());
+		pending.setPoints(stepper.getPending());
 		log.d("Lines updated");
-	}
-
-	private void checkInputIndex(float index) {
-		if (index < currentEdge || index > routeSize - 1) {
-			throw new IllegalArgumentException("Index must be between "
-					+ currentEdge + " and " + (routeSize - 1));
-		}
-	}
-
-	private LatLng linearInterpolate(LatLng p1, LatLng p2, float coeff) {
-		Log.d("ROUTE", "coeff: " + coeff);
-		double lat = p1.latitude + coeff * (p2.latitude - p1.latitude);
-		double lng = p1.longitude + coeff * (p2.longitude - p1.longitude);
-		return new LatLng(lat, lng);
 	}
 
 	private List<LatLng> convertPointList(List<Point> input) {
---
+++ route-commons/trunk/src/it/polimi/dima/dacc/mountainroute/commons/presentation/RouteStepper.java
@@ -0,0 +1,112 @@
+package it.polimi.dima.dacc.mountainroute.commons.presentation;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class RouteStepper {
+
+	private LinkedList<LatLng> traversedRoute;
+	private LinkedList<LatLng> pendingRoute;
+
+	private float currentTraversedIndex;
+	private int currentPivotIndex;
+	private boolean hasInterpolationPoint;
+
+	public RouteStepper(List<LatLng> route, float traversedIndex) {
+		if (traversedIndex > route.size() - 1) {
+			throw new RuntimeException(
+					"Traversed index is greater that route size!");
+		}
+
+		this.traversedRoute = new LinkedList<LatLng>();
+		this.pendingRoute = new LinkedList<LatLng>();
+		this.currentTraversedIndex = traversedIndex;
+		this.currentPivotIndex = parseInt(currentTraversedIndex);
+
+		// Load Rout
+		int routeLength = route.size();
+		traversedRoute.addAll(route.subList(0, currentPivotIndex + 1));
+		pendingRoute.addAll(route.subList(currentPivotIndex + 1, routeLength));
+		hasInterpolationPoint = false;
+
+		// Add interpolation point
+		float interpolationCoeff = currentTraversedIndex - currentPivotIndex;
+		addInterpolation(interpolationCoeff);
+	}
+
+	public RouteStepper(List<LatLng> route) {
+		this(route, 0);
+	}
+
+	public List<LatLng> getPending() {
+		return pendingRoute;
+	}
+
+	public List<LatLng> getTraversed() {
+		return traversedRoute;
+	}
+
+	public void step(float newIndex) {
+		if (newIndex <= currentTraversedIndex) {
+			throw new RuntimeException("Cannot step back in route");
+		}
+
+		clearInterpolation();
+
+		int newPivotIndex = parseInt(currentTraversedIndex);
+
+		if (newPivotIndex > currentPivotIndex) {
+			// The pivot is present in both list. After the step the old pivot
+			// should be only in the traversed list, the new one in both.
+			pendingRoute.removeFirst();
+
+			// Copy new pivot to traversed list
+			LatLng pivot = pendingRoute.getFirst();
+			pendingRoute.addLast(pivot);
+			currentPivotIndex = newPivotIndex;
+		}
+
+		addInterpolation(newIndex - currentPivotIndex);
+
+	}
+
+	private int parseInt(double d) {
+		return (int) Math.floor(d);
+	}
+
+	private void clearInterpolation() {
+		if (hasInterpolationPoint) {
+			traversedRoute.removeLast();
+			pendingRoute.removeFirst();
+			hasInterpolationPoint = false;
+		}
+	}
+
+	private void addInterpolation(float coefficient) {
+		if (hasInterpolationPoint) {
+			return;
+		}
+
+		if (coefficient == 0) {
+			// The interpolated point would coincide with current pivot, so do
+			// nothing.
+			hasInterpolationPoint = false;
+			return;
+		}
+
+		LatLng interpolated = interpolate(traversedRoute.getLast(),
+				pendingRoute.getFirst(), coefficient);
+		traversedRoute.addLast(interpolated);
+		pendingRoute.addFirst(interpolated);
+		hasInterpolationPoint = true;
+	}
+
+	private LatLng interpolate(LatLng p1, LatLng p2, float coeff) {
+		double lat = p1.latitude + coeff * (p2.latitude - p1.latitude);
+		double lng = p1.longitude + coeff * (p2.longitude - p1.longitude);
+		return new LatLng(lat, lng);
+	}
+
+}
